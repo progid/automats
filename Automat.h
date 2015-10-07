a233213b60188607844b2360662f091a88eb3365
Dmitriy00047 <dima.kibetx@gmail.com>
2015-10-07 16:11:36 +0300

    class Atomat
    Description class Atomat

---
+++ Automat.h
@@ -0,0 +1,9 @@
+#include <list>
+#include "ConditionForAutomat.h"
+
+class Automat {
+	string InputAlphabet;
+	//List of Conditions
+	ConditionForAutomat *BeginCondition;
+	//List of FinalConditions
+};
